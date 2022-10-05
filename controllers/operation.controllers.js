c0d20519d7e36a568c67ba0b4d98796404154a7b
Alejandra179 <alejandra.maza1@gmail.com>
2022-10-05 11:40:56 -0300

    add route to update operation with put

--- controllers/operation.controllers.js
+++ controllers/operation.controllers.js
@@ -6,26 +6,35 @@ ctrl.addOperation = (req, res) => {
   const sql = `INSERT INTO operations (concept, amount, date, type_operation,category) VALUES ("${concept}",${amount},"${date}","${type_operation}","${category}")`;
 
   connection.query(sql, (error, rows) => {
-    (error)? res.status(400).json(error): res.json(rows);
+    error ? res.status(400).json(error) : res.json(rows);
   });
 };
 
 ctrl.getOperationBytype = (req, res) => {
   const { type_operation } = req.params;
-  
+
   const sql = `SELECT * FROM operations WHERE  type_operation = "${type_operation}";`;
 
   connection.query(sql, (err, row) => {
-    (err)?res.status(400).json(err): res.status(200).json(row);
-    
+    err ? res.status(400).json(err) : res.status(200).json(row);
   });
 };
-ctrl.getOperaciones = (req, res) => {
-  let sql = "select * from operaciones";
+ctrl.getOperationes = (req, res) => {
+  let sql = "select * from operations";
   connection.query(sql, (err, row) => {
-    (err)? res.status(400).json(err):res.json(row);
-    
+    err ? res.status(400).json(err) : res.json(row);
   });
 };
 
+ctrl.updateOperation = (req, res) => {
+  const { concept, amount, type_operation, category, date } = req.body;
+  const { id_operation } = req.params;
+
+  let sql = `UPDATE operations SET concept ="${concept}",amount=${amount},
+    type_operation="${type_operation}",category="${category}",date="${date}"  
+    WHERE operations.id_operation = ${id_operation}`;
+  connection.query(sql, (err, row) => {
+    err ? res.status(400).json(err) : res.json(row);
+  });
+}; 
 module.exports = ctrl;
--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,9 @@
-const { getOperaciones, addOperation, getOperationBytype } = require("../controllers/operation.controllers")
+const { getOperationes, addOperation, getOperationBytype, updateOperation } = require("../controllers/operation.controllers")
 
 const router = require("express").Router()
 
-router.get("/",getOperaciones)
+router.get("/",getOperationes)
 router.post("/addOperation",addOperation)
 router.get("/:type_operation",getOperationBytype)
-
+router.put("/:id_operation",updateOperation)
 module.exports = router;
