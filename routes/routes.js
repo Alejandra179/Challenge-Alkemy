81ad3a27bfdaf006ae5c205102726fde0d2f3f1d
Alejandra179 <alejandra.maza1@gmail.com>
2022-10-05 10:58:16 -0300

    operation routes updated

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,9 @@
-const { getOperaciones } = require("../controllers/controller")
+const { getOperaciones, addOperation, getOperationBytype } = require("../controllers/operation.controllers")
 
 const router = require("express").Router()
 
 router.get("/",getOperaciones)
+router.post("/addOperation",addOperation)
+router.get("/:type_operation",getOperationBytype)
 
 module.exports = router;
