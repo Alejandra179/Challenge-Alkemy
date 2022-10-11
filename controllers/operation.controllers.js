5d268b25a51b53a54e24ac7234f1d8420168805b
Alejandra179 <alejandra.maza1@gmail.com>
2022-10-11 15:58:05 -0300

    auth was updated

--- controllers/operation.controllers.js
+++ controllers/operation.controllers.js
@@ -3,9 +3,9 @@ const connection = require("../db/connection");
 
 ctrl.addOperation = (req, res) => {
   const { concept, amount, type_operation, category, date } = req.body;
-  const id_user = req.header('id_user')
+ 
   let sql = `INSERT INTO operations (concept, amount, date, type_operation,category, id_user) 
-  VALUES ("${concept}",${amount},"${date}","${type_operation}","${category}",${id_user})`;
+  VALUES ("${concept}",${amount},"${date}","${type_operation}","${category}",${req.user.id_user})`;
 
   connection.query(sql, (error, rows) => {
     error
@@ -16,10 +16,9 @@ ctrl.addOperation = (req, res) => {
 
 ctrl.getOperationsBytype = (req, res) => {
   const { id_type } = req.params;
-  const id_user = req.header('id_user')
 
   let sql = `SELECT * FROM operations WHERE  type_operation = ${id_type} 
-  and leavingDate is NULL and id_user=${id_user};`;
+  and leavingDate is NULL and id_user=${req.user.id_user};`;
 
   connection.query(sql, (err, row) => {
     err ? res.status(400).json(err) : res.status(200).json(row);
@@ -27,24 +26,24 @@ ctrl.getOperationsBytype = (req, res) => {
 };
 ctrl.getOperationsByCategory = (req, res) => {
   const { category } = req.params;
-  const id_user = req.header('id_user')
-  let sql = `Select * from operations where category="${category}" and leavingDate is NULL and id_user=${id_user} ;`;
+  
+  let sql = `Select * from operations where category="${category}" and leavingDate is NULL and id_user=${req.user.id_user} ;`;
   connection.query(sql, (err, row) => {
     err ? res.status(400).json(err) : res.json(row);
   });
 };
 ctrl.getOperations = (req, res) => {
-  const id_user = req.header('id_user')
-  let sql = `select * from operations WHERE leavingDate is NULL and id_user=${id_user}`;
+
+  let sql = `select * from operations WHERE leavingDate is NULL and id_user=${req.user.id_user}`;
   connection.query(sql, (err, row) => {
     err ? res.status(400).json(err) : res.json(row);
   });
 };
 ctrl.getOperationsByTypeAndCategory = (req, res) => {
   const { category, type_operation } = req.params;
-  const id_user = req.header('id_user')
+ 
   let sql = `Select * from operations where category=${category} and type_operation=${type_operation} and 
-  leavingDate is NULL and id_user=${id_user};`;
+  leavingDate is NULL and id_user=${req.user.id_user};`;
   connection.query(sql, (err, row) => {
     err ? res.status(400).json(err) : res.status(200).json(row);
   });
@@ -52,12 +51,12 @@ ctrl.getOperationsByTypeAndCategory = (req, res) => {
 
 ctrl.updateOperation = (req, res) => {
   const { concept, amount, category, date} = req.body;
-  const id_user = req.header('id_user')
+
   const { id_operation } = req.params;
 
   let sql = `UPDATE operations SET concept ="${concept}",amount=${amount},
    category="${category}",date="${date}"  
-    WHERE operations.id_operation = ${id_operation} and id_user=${id_user}`;
+    WHERE operations.id_operation = ${id_operation} and id_user=${req.user.id_user}`;
   connection.query(sql, (err, row) => {
     err
       ? res.status(400).json(err)
@@ -66,10 +65,9 @@ ctrl.updateOperation = (req, res) => {
 };
 
 ctrl.deleteOperation = (req, res) => {
-  const { id_user } = req.body;
+ 
   const { id_operation } = req.params;
-  let sql = `UPDATE operations SET leavingDate = now() where id_operation = ${id_operation}
-  and id_user=${id_user}`;
+  let sql = `UPDATE operations SET leavingDate = now() where id_operation = ${id_operation}`;
   connection.query(sql, (err, row) => {
     err
       ? res.status(400).json(err)
@@ -79,8 +77,8 @@ ctrl.deleteOperation = (req, res) => {
 
 
 ctrl.getLastOperations = (req, res) => {
-  const id_user = req.header('id_user')
-  let sql = `select * from operations where id_user=${id_user} and leavingDate is NULL 
+  
+  let sql = `select * from operations where id_user=${req.user.id_user} and leavingDate is NULL 
   ORDER BY id_operation DESC LIMIT 10`;
   connection.query(sql, (error, row) => {
     error ? res.status(400).json(error) : res.status(200).json(row);
@@ -88,19 +86,21 @@ ctrl.getLastOperations = (req, res) => {
 };
 
 ctrl.getBalance = (req, res) => {
-  const id_user = req.header('id_user')
-  let sql = `select sum(case when type_operation = 2 then amount else - amount end) as balance from operations where id_user = ${id_user} AND leavingDate is null`;
+  
+  let sql = `select sum(case when type_operation = 2 then amount else - amount end) as balance from operations 
+  where id_user = ${req.user.id_user} AND leavingDate is null`;
   connection.query(sql, (err, row) => {
     err ? res.status(400).json(err) : res.json(row[0].balance);
   });
 };
 
 ctrl.addCategory = (req, res) => {
+  
   const { description } = req.body;
   let sql = `INSERT INTO categories(description) VALUES ("${description}");`;
   connection.query(sql, (err, row) => {
     err
-      ? res.status(400).json(err)
+      ? res.status(400).json(err.message)
       : res.json({ message: "category added successfully" });
   });
 };
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,28 +1,32 @@
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
+const connection = require("../db/connection");
 
-const  SECRET = process.env.SECRET
-module.exports = function(req, res, next)  {
-    //get token f. header
-    const token = req.header('x-auth-token')
-    //check token
-    if(!token){
-        return res.status(401).json({msg: 'no token, unauthorized' })
-    }
+const SECRET = process.env.SECRET;
+module.exports = function (req, res, next) {
+  //get token f. header
+  const token = req.header("x-auth-token");
+  //check token
+  if (!token) {
+    return res.status(401).json({ msg: "no token, unauthorized" });
+  }
 
-    try {
-        //modificar jwtSecret
-        const decoded = jwt.verify(token, SECRET)
-
-        //console.log('DECODED => ', decoded)
-
-        //attach user at req and pass to next mware.
-        req.user = decoded.user 
-
-        next()
-
-    } catch (error) {
-        res.status(401).json({msg: 'unvalid token'})
-    }
-
-
-}
+  try {
+    //modificar jwtSecret
+    const decoded = jwt.verify(token, SECRET);
+   console.log(decoded)
+    //console.log('DECODED => ', decoded)
+    let sql = `select * from users where id_user="${decoded.uid}"`;
+    connection.query(sql, (err, row) => {
+      if(err) return res.json(err.message) 
+      if(row.length === 0){        
+        res.json({message:'Token invalid - user not found'})
+      } else{
+          req.user = row[0]
+          next()
+      }
+    })
+    
+  } catch (error) {
+    res.status(401).json({ msg: "unvalid token" });
+  }
+};
